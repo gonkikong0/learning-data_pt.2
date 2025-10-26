06b47172a1c126ab06ac7a606aa67d506da08804
gonkikong0 <toshivy@outlook.com>
2025-10-26 15:06:50 +0530

    Error Handling for missing data

---
+++ sitka_highs_lows.py
@@ -0,0 +1,42 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'sitka_weather_2018_simple.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header = next(reader)
+   
+
+    # for index, column_header in enumerate(header):
+    #     print (index, column_header) #Data we need Column 2,5
+
+    dates, highs, lows = [] ,[], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        low = int(row[6])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+    #plotting the temperatures (high and low)
+    plt.style.use('seaborn-v0_8-colorblind')
+    fig, ax = plt.subplots()
+    ax.plot(dates,highs, c = "red", alpha = 0.5)
+    ax.plot(dates,lows, c = "blue", alpha = 0.5)
+    plt.fill_between(dates, highs, lows, facecolor = 'blue', alpha = 0.1)
+
+    
+    
+
+
+    #formatting the plot
+    plt.title("Daily high temperatures, July2018", fontsize = 24)
+    plt.xlabel('', fontsize = 16)
+    fig.autofmt_xdate()
+    plt.ylabel('Temperature(F)', fontsize = 16)
+    plt.tick_params(axis = 'both', which='major', labelsize = 16)
+
+    plt.show()
